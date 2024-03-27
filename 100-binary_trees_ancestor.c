1f61cb971a8afaf955ae073e244ca30a5385cf2f
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-03-27 12:12:10 +0300

    revision

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,7 +5,7 @@
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
  *
- * Return: If no common ancestors return NULL, else return common ancestor.
+ * Return: If no common ancestor NULL, else common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
